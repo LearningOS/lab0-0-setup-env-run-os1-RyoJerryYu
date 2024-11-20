e20d426405cedded0d4d9f2847c310551cf18826
RyoJerryYu <tzl19961216@gmail.com>
2024-11-21 01:26:27 +0800

    Impl logging for ch1

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,16 +2,24 @@
 #![no_main]
 mod lang_items;
 mod sbi;
+mod logging;
 #[macro_use]
 mod console;
 
 use core::arch::global_asm;
+
+use log::{info, trace};
+
 global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    println!("Hello, RISC-V!");
+    logging::init();
+    println!("\x1b[31mHello, RISC-V!\x1b[0m");
+
+    trace!("Hello, RISC-V!");
+    info!("Hello, RISC-V!");
     panic!("oops!");
     // sbi::shutdown(false);
 }
