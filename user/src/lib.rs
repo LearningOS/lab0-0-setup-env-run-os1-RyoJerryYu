bdd1514a08a3f7b4baf5e131847c2007530ce5a6
RyoJerryYu <tzl19961216@gmail.com>
2024-12-04 00:21:13 +0800

    add syscall get time for client

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -3,6 +3,8 @@
 #![feature(linkage)]
 #![feature(panic_info_message)]
 
+use syscall::sys_get_time;
+
 #[macro_use]
 pub mod console;
 mod lang_items;
@@ -38,4 +40,7 @@ pub fn exit(code: i32) -> ! {
 }
 pub fn yield_() {
     syscall::sys_yield()
+}
+pub fn get_time() -> isize {
+    sys_get_time()
 }
