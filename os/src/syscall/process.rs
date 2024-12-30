e05c4ec6a8e63c1d9abef4aaae45ae79872377a8
RyoJerryYu <tzl19961216@gmail.com>
2024-12-31 00:56:55 +0800

    feat: add sys get pid

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,8 @@
+use alloc::sync::Arc;
+
 use crate::{
     loaders::get_app_data_by_name,
-    mm::translated_str,
+    mm::{translated_refmut, translated_str},
     println,
     task::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
@@ -23,6 +25,11 @@ pub fn sys_yield() -> isize {
 pub fn sys_get_time() -> isize {
     get_time_ms() as isize
 }
+
+pub fn sys_getpid() -> isize {
+    current_task().unwrap().pid.0 as isize
+}
+
 pub fn sys_fork() -> isize {
     let current_task = current_task().unwrap();
     let new_task = current_task.fork();
@@ -46,5 +53,39 @@ pub fn sys_exec(path: *const u8) -> isize {
         0
     } else {
         -1
+    }
+}
+
+pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
+    let current_task = current_task().unwrap();
+    let mut current_task_inner = current_task.inner_xclusive_access();
+    if current_task_inner
+        .children
+        .iter()
+        .find(|p| pid == -1 || pid as usize == p.getpid())
+        .is_none()
+    {
+        return -1;
+    }
+
+    let pair = current_task_inner
+        .children
+        .iter()
+        .enumerate()
+        .find(|(_, p)| {
+            p.inner_xclusive_access().is_zombie() && (pid == -1 || pid as usize == p.getpid())
+        });
+
+    if let Some((index, _)) = pair {
+        let child = current_task_inner.children.remove(index);
+        assert_eq!(Arc::strong_count(&child), 1);
+
+        let found_pid = child.getpid();
+        let exit_code = child.inner_xclusive_access().exit_code;
+
+        *translated_refmut(current_task_inner.memory_set.token(), exit_code_ptr) = exit_code;
+        found_pid as isize
+    } else {
+        -2
     }
 }
