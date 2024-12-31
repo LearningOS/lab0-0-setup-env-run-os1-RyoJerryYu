ad488e0fb7c916feca4773bdab5e0d6c1ca65983
RyoJerryYu <tzl19961216@gmail.com>
2024-12-31 23:35:22 +0800

    fix: phys page frame did not clear

--- os/src/mm/frame_allocator.rs
+++ os/src/mm/frame_allocator.rs
@@ -20,6 +20,11 @@ impl Debug for FrameTracker {
 
 impl FrameTracker {
     pub fn new(ppn: PhysPageNum) -> Self {
+        // page cleaning
+        let bytes_array = ppn.get_bytes_array();
+        for i in bytes_array {
+            *i = 0;
+        }
         Self { ppn }
     }
 }
