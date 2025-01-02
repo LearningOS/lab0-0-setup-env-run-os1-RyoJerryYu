4d351abe982bdf1baf103af64ac9990ccc0ec43a
RyoJerryYu <tzl19961216@gmail.com>
2025-01-02 23:32:18 +0800

    feat: add sys open and close to user lib

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -3,6 +3,7 @@
 #![feature(linkage)]
 #![feature(panic_info_message)]
 
+use bitflags::bitflags;
 use buddy_system_allocator::LockedHeap;
 use syscall::*;
 
@@ -32,6 +33,24 @@ pub extern "C" fn _start() -> ! {
 #[no_mangle]
 fn main() -> i32 {
     panic!("Cannot find main!");
+}
+
+bitflags! {
+    pub struct OpenFlags: u32 {
+        const RDONLY = 0;
+        const WRONLY = 1 << 0;
+        const RDWR = 1 << 1;
+        const CREATE = 1 << 9;
+        const TRUNC = 1 << 10;
+    }
+}
+
+pub fn open(path: &str, flags: OpenFlags) -> isize {
+    sys_open(path, flags.bits as usize)
+}
+
+pub fn close(fd: usize) -> isize {
+    sys_close(fd)
 }
 
 pub fn read(fd: usize, buf: &mut [u8]) -> isize {
